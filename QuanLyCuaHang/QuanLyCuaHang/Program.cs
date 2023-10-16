f1363c4ed11f1b7f6d6684b4d2ad94a9bed9c94d
vanphuoc1029 <111067471+vanphuoc1029@users.noreply.github.com>
2023-10-16 19:29:08 +0700

    Add files via upload

---
+++ QuanLyCuaHang/QuanLyCuaHang/Program.cs
@@ -0,0 +1,33 @@
+﻿public struct item
+{
+    public int id, namsx;
+    public string ten, congtysx, loaihang, hansd;
+}
+internal class program
+{
+ 
+
+    public static item themSanPham() 
+    {
+        item new_item;
+        Console.Write("Nhap ID san pham: ");
+        new_item.id = int.Parse(Console.ReadLine());
+        Console.Write("Nhap ten san pham: ");
+        new_item.ten = Console.ReadLine();
+        Console.Write("Nhap nam san suat: ");
+        new_item.namsx = int.Parse(Console.ReadLine());
+        Console.Write("Nhap han su dung: ");
+        new_item.hansd = Console.ReadLine();
+        Console.Write("Nhap cong ty san suat: ");
+        new_item.congtysx = Console.ReadLine();
+        Console.Write("Nhap loai hang: ");
+        new_item.loaihang = Console.ReadLine();
+        return new_item;
+
+    }
+
+    static void Main (string[] args)
+    {
+        item sanphama = themSanPham();
+    }
+}
