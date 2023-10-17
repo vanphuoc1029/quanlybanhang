13a2ee7521e6ed64df9aec5ea3d8ea6b268a2047
Zenbook 14X <Zenbook 14X@LAPTOP-PAAQLK6H>
2023-10-17 16:00:31 +0700

    #update

--- QuanLyCuaHang/QuanLyCuaHang/Program.cs
+++ QuanLyCuaHang/QuanLyCuaHang/Program.cs
@@ -1,18 +1,21 @@
-﻿public struct item
+﻿using System.Xml.Serialization;
+namespace QuanLyCuaHang;
+public struct item
 {
     public int id, namsx;
     public string ten, congtysx, loaihang, hansd;
 }
 internal class program
 {
- 
 
-    public static item themSanPham() 
+
+    public static item themMatHang(List<item> listMatHang)
     {
+
         item new_item;
-        Console.Write("Nhap ID san pham: ");
+        Console.Write("Nhap ID mat hang: ");
         new_item.id = int.Parse(Console.ReadLine());
-        Console.Write("Nhap ten san pham: ");
+        Console.Write("Nhap ten mat hang: ");
         new_item.ten = Console.ReadLine();
         Console.Write("Nhap nam san suat: ");
         new_item.namsx = int.Parse(Console.ReadLine());
@@ -22,12 +25,48 @@ internal class program
         new_item.congtysx = Console.ReadLine();
         Console.Write("Nhap loai hang: ");
         new_item.loaihang = Console.ReadLine();
+        listMatHang.Add(new item());
         return new_item;
 
     }
 
-    static void Main (string[] args)
+ 
+    static void Main(string[] args)
     {
-        item sanphama = themSanPham();
+        List<item> listMatHang = new List<item>();
+        List<string> listLoaiHang = new List<string>();
+        while (true)
+        {
+            Console.WriteLine("Moi ban chon tinh nang: \n1. Quan ly loai hang\n2. Quan ly mat hang" +
+            "\n3. Thoat chuong trinh\n");
+            string choice = Console.ReadLine();
+            {
+                if (choice == "1")
+                {
+                    bool quanLyLoaiHan = true;
+                    while (quanLyLoaiHan)
+                    {
+                        Console.WriteLine("Moi ban chon tinh nang quan ly loai hang:\n1. Them" +
+                            "\n2. Xoa loai hang\n3. Sua\n4. Tim kiem\n5. Quay lai\n  ");
+                        int categoryChoice = int.Parse(Console.ReadLine());
+                        switch (categoryChoice)
+                        {
+                            case 1:
+                                XuLyLoaiHang.themLoaiHang(listLoaiHang);
+                                break;
+                            case 2:
+                                XuLyLoaiHang.xoaLoaiHang(listLoaiHang);
+                                break;
+                            case 3:
+                                XuLyLoaiHang.suaLoaiHang(listLoaiHang);
+                                break;
+
+                        }
+
+                    }
+
+                }
+            }
+        }
     }
 }
---
+++ QuanLyCuaHang/QuanLyCuaHang/XuLyLoaiHang.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHang
+{
+    internal class XuLyLoaiHang
+    {
+        public static void themLoaiHang(List<string> listLoaiHang)
+        {
+            Console.WriteLine("Nhap ten loai hang: ");
+            listLoaiHang.Add(Console.ReadLine());
+
+        }
+
+        public static bool kiemTraListTrong(List<string> listLoaiHang)
+        {
+            if (listLoaiHang.Count == 0)
+            {
+                Console.WriteLine("Hien danh sach loai hang dang trong");
+                return true;
+            }
+            return false;
+        }
+
+        public static void inList(List<string> listLoaiHang)
+        {   
+            int i = 0;
+            foreach (string item in listLoaiHang)
+            {
+                Console.WriteLine($"{i}. {item}");
+                
+                i++;
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static bool checkValidChoice(int choice, List<string> listLoaiHang)
+        {
+            if (choice < 0 || choice > (listLoaiHang.Count - 1)) 
+            {
+                Console.WriteLine("So thu tu khong hop le");
+                return false;
+            }
+            return true;
+        }
+        public static void xoaLoaiHang(List<string> listLoaiHang)
+        {
+            bool nullList = kiemTraListTrong(listLoaiHang);
+
+            if (!nullList)
+            {
+                Console.WriteLine("Chon so thu tu loai hang ban muon xoa");
+                int i = 1;
+                inList(listLoaiHang);
+                int choice = int.Parse(Console.ReadLine());
+                bool validChoice = checkValidChoice(choice, listLoaiHang);
+                if (validChoice)
+                {
+                    listLoaiHang.RemoveAt(choice);
+                }
+                
+            }
+        }
+
+        public static void suaLoaiHang(List<string> listLoaiHang)
+        {
+            bool nullList = kiemTraListTrong(listLoaiHang);
+            if (!nullList)
+            {
+                Console.WriteLine("Moi ban chon so thu tu muon sua: ");
+                int i = 1;
+                inList(listLoaiHang);
+                int choice = int.Parse(Console.ReadLine());
+                bool validChoice = checkValidChoice(choice, listLoaiHang);
+                if (validChoice)
+                {
+                    Console.Write("Nhap noi dung thay the: ");
+                    listLoaiHang[choice] = Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
